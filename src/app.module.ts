b34c8b424a8bf2f6cb8b9837ba2e2abffcd49b51
const <epitoneplus@gmail.com>
2022-01-18 16:07:27 +0900

    [chore]: 동적 모듈을 활용한 환경변수 구성 (#18)
    1. ConfigModule을 활용한 환경변수 구성
    2. Joi를 활용한 환경변수 유효성 검사
    3. .env 파일을 development, stage, production으로 구분
    4. 커스텀 Config 파일 생성해서 환경변수를 의미있는 단위로 묶어서 처리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { ExceptionModule } from './common/exceptions/exception.module';
-import { LoggingModule } from './common/logging/logging.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingModule } from './common/logging/logging.module';
+import { ExceptionModule } from './common/exceptions/exception.module';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/validationSchema';
+import authConfig from './config/authConfig';
+import databaseConfig from './config/databaseConfig';
 
 @Module({
-  imports: [LoggingModule, ExceptionModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [databaseConfig, authConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+    LoggingModule,
+    ExceptionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
