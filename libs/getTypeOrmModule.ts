2f58d9e44478aa45c732bea6c3ff9ed86f189e0f
찬기 <limchangi@naver.com>
2022-01-30 10:15:57 +0900

    [chore]: DB TypeOrm Modyle 설정

---
+++ libs/getTypeOrmModule.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import * as path from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const getRealTypeOrmModule = () => {
+  const entityPath = path.join(__dirname, 'entity/src/domain/**/*.entity.ts');
+  return TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService): TypeOrmModuleOptions =>
+      Object.assign(configService.get<TypeOrmModuleOptions>('database'), {
+        type: 'postgres',
+        entities: [entityPath],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        namingStrategy: new SnakeNamingStrategy(),
+      }),
+
+    inject: [ConfigService],
+  });
+};
