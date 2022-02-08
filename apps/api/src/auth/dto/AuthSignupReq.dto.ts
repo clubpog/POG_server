10c1c62e28957152ff38525f6867f0dd240ec06e
epitone <epitoneplus@gmail.com>
2022-02-08 20:04:38 +0900

    [chore]: swagger 사용 로직 구성
    1. @nestjs/swagger, swagger-ui-express 라이브러리를 설치하여 swagger 환경 구성
    2. express-basic-auth 라이브러리를 설치하여 swagger 보안 환경 구성
    3. API 문서 작성

--- apps/api/src/auth/dto/AuthSignupReq.dto.ts
+++ apps/api/src/auth/dto/AuthSignupReq.dto.ts
@@ -1,23 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@app/entity/domain/user/User.entity';
 import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class AuthSignupReq {
+  @ApiProperty({
+    example: 'test',
+    description: '회원 가입시 입력 받는 아이디입니다.',
+    required: true,
+  })
   @Expose()
   @IsNotEmpty()
   @IsString()
   userId: string;
 
+  @ApiProperty({
+    example: 'Test123!',
+    description:
+      '회원 가입시 입력 받는 비밀번호입니다. 하나 이상의 소문자, 대문자, 숫자 및 특수 문자를 포함하는 8~20자 비밀번호를 입력하세요',
+    required: true,
+  })
   @Expose()
   @IsNotEmpty()
   @IsString()
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/,
+    {
+      message:
+        '하나 이상의 소문자, 대문자, 숫자 및 특수 문자를 포함하는 8~20자 비밀번호를 입력하세요',
+    },
+  )
   password: string;
 
+  @ApiProperty({
+    example: 'test',
+    description: '회원 가입시 입력 받는 deviceId입니다.',
+    required: true,
+  })
   @Expose()
   @IsNotEmpty()
   @IsString()
   deviceId: string;
 
+  @ApiProperty({
+    example: 'test',
+    description: '회원 가입시 입력 받는 firebaseToken입니다.',
+    required: true,
+  })
   @Expose()
   @IsNotEmpty()
   @IsString()
---
+++ libs/common-config/src/response/error/BadRequestError.ts
@@ -0,0 +1,29 @@
+import { CustomValidationError } from '@app/common-config/filter/CustomValidationError';
+import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
+
+@ApiExtraModels()
+export class BadRequestError {
+  @ApiProperty({
+    type: 'number',
+    description: 'HTTP Error Code입니다.',
+    example: 'BAD_PARAMETER',
+  })
+  statusCode: number;
+
+  @ApiProperty({
+    type: 'array',
+    title: 'Error 메시지',
+    example: '요청 값에 문제가 있습니다.',
+    description:
+      'Request-Body의 각각 프로퍼티의 제약조건에 맞지 않은 값이 포함되어 있습니다. ' +
+      '해당 예시에 나와 있는 오류중에 적어도 한 개의 조건에 맞지 않아 요청이 실패했습니다.',
+  })
+  message: any;
+
+  @ApiProperty({
+    type: [CustomValidationError],
+    description: 'Error의 이름',
+    example: CustomValidationError,
+  })
+  data: object;
+}
