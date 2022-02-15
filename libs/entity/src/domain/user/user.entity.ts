86d651c47d9eda9b20ec2eaa35d8cdbb658da281
epitone <epitoneplus@gmail.com>
2022-02-15 11:38:45 +0900

    [remove]: 불필요한 라이브러리 주석 처리

--- libs/entity/src/domain/user/user.entity.ts
+++ libs/entity/src/domain/user/user.entity.ts
@@ -1,7 +1,7 @@
 import { Exclude } from 'class-transformer';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { BaseTimeEntity } from '../BaseTimeEntity';
-import { genSalt, hash } from 'bcrypt';
+// import { genSalt, hash } from 'bcrypt';
 import { Favorite } from '../favorite/Favorite.entity';
 
 @Entity()
