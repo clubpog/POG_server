3d03cfa5774898e4e77dfca1e6efac4ae0bd320f
epitone <epitoneplus@gmail.com>
2022-02-18 22:53:36 +0900

    [remove]: 불필요한 모듈 삭제
    1. UserQueryRepository, UserName 파일 삭제
    2. UserModule의 설정 삭제

--- libs/entity/src/domain/user/UserModule.ts
+++ libs/entity/src/domain/user/UserModule.ts
@@ -1,10 +1,9 @@
 import { User } from '@app/entity/domain/user/User.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { UserQueryRepository } from '@app/entity/domain/user/UserQueryRepository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserQueryRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   exports: [TypeOrmModule],
   providers: [],
   controllers: [],
