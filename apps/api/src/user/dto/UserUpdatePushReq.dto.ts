16c717cfca2b6f9b2b3808b5316d8ac1a3b41f5c
epitone <epitoneplus@gmail.com>
2022-02-21 17:10:51 +0900

    [fix]: 푸시알림 허용 여부 변경 API 로직 수정
    1. 입력값에서 deviceId를 받는 것에서, 이를 삭제하고 토큰으로 받게끔 변경
    2. 컨트롤러에서 서비스로 넘어갈 때 dto -> Entity 변경이 아닌, 서비스 계층에서 Entity 변경

--- apps/api/src/user/dto/UserUpdatePushReq.dto.ts
+++ apps/api/src/user/dto/UserUpdatePushReq.dto.ts
@@ -1,19 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { User } from '@app/entity/domain/user/User.entity';
 import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsBoolean, IsString } from 'class-validator';
+import { IsNotEmpty, IsBoolean } from 'class-validator';
 
 export class UserUpdatePushReq {
-  @ApiProperty({
-    example: 'test',
-    description: '푸시알림 허용 여부 변경을 위해 입력 받는 deviceId입니다.',
-    required: true,
-  })
-  @Expose()
-  @IsNotEmpty()
-  @IsString()
-  deviceId: string;
-
   @ApiProperty({
     example: true,
     description: '푸시알림 허용 여부 변경을 위해 입력 받는 isPush입니다.',
@@ -23,8 +12,4 @@ export class UserUpdatePushReq {
   @IsNotEmpty()
   @IsBoolean()
   isPush: boolean;
-
-  toEntity(): Promise<User> {
-    return User.updatePush(this.deviceId, this.isPush);
-  }
 }
