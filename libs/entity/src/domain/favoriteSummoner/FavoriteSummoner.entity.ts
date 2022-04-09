0cc2ae2571f509b2c2ef3b616749ca5d09a0935c
epitone <epitoneplus@gmail.com>
2022-04-10 00:42:30 +0900

    POG-183 [fix]: 즐겨찾기 조회 API 쿼리 튜닝
    1. 불필요한 인덱스 설정이 있었음.
    2. Seq Scan에서 Index Scan으로 되게끔 설정하여 1000만건 데이터 조회시 1.5초 걸리던 쿼리를 0.01초 걸리는 쿼리로 수정

--- libs/entity/src/domain/favoriteSummoner/FavoriteSummoner.entity.ts
+++ libs/entity/src/domain/favoriteSummoner/FavoriteSummoner.entity.ts
@@ -4,8 +4,7 @@ import { SummonerRecord } from '../summonerRecord/SummonerRecord.entity';
 import { User } from '../user/User.entity';
 
 @Entity()
-@Index('idx_favoriteSummoner_1', ['SummonerRecord'])
-@Index('idx_favoriteSummoner_2', ['User'])
+@Index('idx_favoriteSummoner_1', ['User'])
 export class FavoriteSummoner extends BaseTimeEntity {
   @ManyToOne(() => User, (user: User) => user.FavoriteSummoner, {
     onDelete: 'CASCADE',
