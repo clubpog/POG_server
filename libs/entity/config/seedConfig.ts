289eac4c4409029966ff99c9e7342bc4c4aa748e
epitone <epitoneplus@gmail.com>
2022-04-10 00:40:07 +0900

    POG-183 [fix]: seeding, migration 설정 변경
    1. migration 경로 변경
    2. seeding 경로 변경

--- libs/entity/config/seedConfig.ts
+++ libs/entity/config/seedConfig.ts
@@ -7,9 +7,9 @@ dotenv.config();
 const seedConfig = {
   type: 'postgres',
   database: process.env.DB_TEST_NAME,
-  entities: [path.join(__dirname, 'src/domain/**/*.entity.{js,ts}')],
-  seeds: [path.join(__dirname, 'seeds/*.{js,ts}')],
-  factories: [path.join(__dirname, 'factories/*.{js,ts}')],
+  entities: [path.join(__dirname, '..', 'src/domain/**/*.entity.{js,ts}')],
+  seeds: [path.join(__dirname, '..', 'seeds/*.{js,ts}')],
+  factories: [path.join(__dirname, '..', 'factories/*.{js,ts}')],
   namingStrategy: new SnakeNamingStrategy(),
 };
 export default seedConfig;
