f9e80a570f23946bd38d5228c9912861abaa95d9
epitone <epitoneplus@gmail.com>
2022-04-11 22:54:58 +0900

    POG-193 [chore]: 환경 변수 분리
    1. 배포 전, development, production 별로 다르게 동작하도록 설정

--- libs/entity/config/ormConfig.ts
+++ libs/entity/config/ormConfig.ts
@@ -5,25 +5,41 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const [logging, synchronize] =
+const [host, port, username, password, database, logging, synchronize] =
   process.env.NODE_ENV === 'production'
-    ? [process.env.LOGGING, process.env.SYNCHRONIZE]
-    : [process.env.TEST_LOGGING, process.env.TEST_SYNCHRONIZE];
+    ? [
+        process.env.DB_HOST,
+        process.env.DB_PORT,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        process.env.DB_NAME,
+        process.env.LOGGING,
+        process.env.SYNCHRONIZE,
+      ]
+    : [
+        process.env.DB_TEST_HOST,
+        process.env.DB_TEST_PORT,
+        process.env.DB_TEST_USERNAME,
+        process.env.DB_TEST_PASSWORD,
+        process.env.DB_TEST_NAME,
+        process.env.TEST_LOGGING,
+        process.env.TEST_SYNCHRONIZE,
+      ];
 
 const OrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_TEST_HOST,
-  port: Number(process.env.DB_TEST_PORT),
-  username: process.env.DB_TEST_USERNAME,
-  password: process.env.DB_TEST_PASSWORD,
-  database: process.env.DB_TEST_NAME,
+  host,
+  port: Number(port),
+  username,
+  password,
+  database,
   entities: [path.join(__dirname, '..', 'src/domain/**/*.entity.{js,ts}')],
   migrations: [path.join(__dirname, '..', 'migrations/*{.ts,.js}')],
   cli: { migrationsDir: 'libs/entity/migrations' },
   migrationsTableName: 'migrations',
   autoLoadEntities: true,
-  synchronize: synchronize === 'false' ? false : Boolean(synchronize),
-  logging: logging === 'false' ? false : Boolean(logging),
+  synchronize: Boolean(synchronize),
+  logging: Boolean(logging),
   keepConnectionAlive: true,
   namingStrategy: new SnakeNamingStrategy(),
   maxQueryExecutionTime: Number(process.env.DB_CONNECTION_TIMEOUT),
