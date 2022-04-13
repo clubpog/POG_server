4d9398a491d242d7844349c8294dd8b8e39b3ef8
const <epitoneplus@gmail.com>
2022-04-13 22:07:47 +0900

    [HOTFIX]: 솔로 랭크 경기 전적에 맞게만 푸시 알림을 보내게끔 변경 (#140)
    1. 기존에는 솔로 랭크 전적의 데이터만 다루지 않았던 문제가 있었음.
    2. 급한 문제 해결하고 코드에서 나는 악취를 해결할 예정

--- libs/common-config/src/job/RiotApi.ts
+++ libs/common-config/src/job/RiotApi.ts
@@ -9,5 +9,10 @@ export const RiotApiJobs = async (summonerId: string) => {
   const url = `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${api_key}`;
 
   const res = await axios.get(url);
-  return res.data[0];
+
+  for (let i = 0; i < res.data.length; i++) {
+    if (res.data[i]['queueType'] === 'RANKED_SOLO_5x5') {
+      return res.data[i];
+    }
+  }
 };
