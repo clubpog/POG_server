e6eea71d541f1d15791ce365e5831e0070d6ca2d
const <epitoneplus@gmail.com>
2022-04-22 22:22:56 +0900

    [fix]: 소환사 즐겨찾기 조회 API 에러 로직 처리 변경된 API 추가 (#158)
    ## 작업사항
    1. 소환사 즐겨찾기 조회 API 에러 핸들링 로직 추가
    2. 단위 테스트 실패 경우 추가
    
    ## 관계된 이슈, PR 
    #146

---
+++ libs/common-config/src/response/swagger/domain/favoriteSummoner/FavoriteSummonerReadFailV1.ts
@@ -0,0 +1,23 @@
+import { InternalServerError } from '@app/common-config/response/swagger/common/error/InternalServerError';
+import { ApiExtraModels, ApiProperty, PickType } from '@nestjs/swagger';
+
+@ApiExtraModels()
+export class FavoriteSummonerReadFailV1 extends PickType(InternalServerError, [
+  'statusCode',
+] as const) {
+  @ApiProperty({
+    type: 'string',
+    title: '실패 메시지',
+    example: 'Internal Server Error',
+    description: '소환사 즐겨찾기 조회에 실패했습니다.',
+  })
+  message: string;
+
+  @ApiProperty({
+    type: 'string',
+    title: '실패 메시지',
+    example: 'Internal Server Error',
+    description: '소환사 즐겨찾기 조회에 실패했습니다.',
+  })
+  data: string;
+}
