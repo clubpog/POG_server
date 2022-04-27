6db4d2a0febc30c94e509e0bdd677a37402851da
epitone <epitoneplus@gmail.com>
2022-04-27 15:34:10 +0900

    POG-235 [fix]: redis를 활용하는 단위테스트 stub 객체 활용 변경

--- libs/entity/test/stub/RedisServiceStub.ts
+++ libs/entity/test/stub/RedisServiceStub.ts
@@ -1,9 +1,35 @@
-import { RedisStub } from './RedisStub';
+import { IEventStoreService } from './../../../cache/interface/integration';
+import { FavoriteSummonerReq } from './../../../../apps/api/src/favoriteSummoner/dto/FavoriteSummonerReq.dto';
 
-export class RedisServiceStub {
-  private readonly master;
-
-  constructor() {
-    this.master = new RedisStub();
+export class RedisServiceStub implements IEventStoreService {
+  async set(): Promise<void> {}
+  async save(): Promise<void> {}
+  async sadd(): Promise<void> {}
+  async get(): Promise<string> {
+    return 'OK';
   }
+
+  // async del(array): Promise<any> {
+  //   return;
+  // }
+
+  // async mget(...args): Promise<null[]> {
+  //   return [null, null, null];
+  // }
+
+  // async multi() {
+  //   return;
+  // }
+
+  // async exec(): Promise<boolean> {
+  //   return null;
+  // }
+
+  // async srem(): Promise<void> {}
+
+  async saveRedisSummonerRecord(
+    favoriteSummonerDto: FavoriteSummonerReq,
+  ): Promise<void> {}
+
+  async removeTransactionRedis(summonerId: string): Promise<void> {}
 }
