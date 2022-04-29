5c2388dfa22349425dc82f204e47f948649c4e6f
changi <limchangi@naver.com>
2022-04-29 15:56:42 +0900

    [test]: Code Deploy test

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
 REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 rm -rf node_modules
-npm ci
+npm install
 npm run build
 pm2 start dist/apps/api/src/main.js --name "pog-push"
