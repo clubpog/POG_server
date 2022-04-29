c0452c872f5a5d65b42b8c98454b9e09308b7e17
changi <limchangi@naver.com>
2022-04-29 14:02:33 +0900

    [test]: Code Deploy test

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -6,7 +6,6 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 rm -rf node_modules
-rm package-lock.json
-npm install --save --legacy-peer-deps
+npm ci
 npm run build
 pm2 start dist/apps/api/src/main.js --name "pog-push"
