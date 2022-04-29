15bd4416466cf3507157a12ffb0ba963595fcc61
changi <limchangi@naver.com>
2022-04-29 16:07:32 +0900

    [test]: Code Deploy test sudo

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,6 +3,6 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 rm -rf node_modules
-npm install
-npm run build
-pm2 start dist/apps/api/src/main.js --name "pog-push"
+sudo npm install
+sudo npm run build
+sudo pm2 start dist/apps/api/src/main.js --name "pog-push"
